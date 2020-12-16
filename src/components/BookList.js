be874ed55891152a7cb05de3666d84c26c8b4670
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-16 19:29:39 +0200

    Add th for the booklist component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,3 +1,19 @@
-const BookList = () => {};
+const BookList = () => (
+  <div>
+    <table>
+      <thead>
+        <th>
+          Book ID
+        </th>
+        <th>
+          Title
+        </th>
+        <th>
+          Category
+        </th>
+      </thead>
+    </table>
+  </div>
+);
 
 export default BookList;
