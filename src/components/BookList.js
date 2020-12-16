4e462e98c567d0c2d515ae398bdb6d3e38d96bb1
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-16 19:51:00 +0200

    Extract booklist from the redux store

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,29 +1,38 @@
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const BookList = () => (
-  <div>
-    <table>
-      <thead>
-        <th>
-          Book ID
-        </th>
-        <th>
-          Title
-        </th>
-        <th>
-          Category
-        </th>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Sample book</td>
-          <td>Tech books</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+const BookList = ({ books }) => {
+  const { bookId, bookTitle, bookCategory } = books;
+  return (
+    <div>
+      <table>
+        <thead>
+          <th>
+            Book ID
+          </th>
+          <th>
+            Title
+          </th>
+          <th>
+            Category
+          </th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{bookId}</td>
+            <td>{bookTitle}</td>
+            <td>{bookCategory}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+BookList.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  books: PropTypes.object.isRequired,
+};
 
 const mapStateToProps = state => ({ books: state.books });
 
