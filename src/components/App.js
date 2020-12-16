b8f62ce6978c7a18f5af28b5eb7297a9ab448d70
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-16 03:44:51 +0200

    Setup all linters and fix some issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
-import {Fragment} from 'react';
+import { Fragment } from 'react';
 
-const App = () => {
-  return (
-    <Fragment className="App">
-      <h1>Hello, Welcome to the Bookstore project developed with react && redux</h1>
-    </Fragment>
-  );
-}
+const App = () => (
+  <Fragment className="App">
+    <h1>Hello, Welcome to the Bookstore project developed with react && redux</h1>
+  </Fragment>
+);
 
 export default App;
