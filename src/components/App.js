f771bdada9d0b8419a51b900a3fc20656ee630ee
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-16 03:25:20 +0200

    Create and add all basics initial codes

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import {Fragment} from 'react';
+
+const App = () => {
+  return (
+    <Fragment className="App">
+      <h1>Hello, Welcome to the Bookstore project developed with react && redux</h1>
+    </Fragment>
+  );
+}
+
+export default App;
