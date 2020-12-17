0ded7427455a0ed8b06e445a73f45ac2ffab29b2
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-17 13:16:58 +0200

    Add form for creating a book

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,3 +1,12 @@
-const BooksForm = () => {};
+const BooksForm = () => (
+  <div>
+    <form>
+      <input type="text" placeholder="Enter the name of a book" />
+      <select>
+        <option value="">---Select the book category---</option>
+      </select>
+    </form>
+  </div>
+);
 
 export default BooksForm;
