cdb109009eaea0ac201e6ae188b316b0a692d5aa
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-18 00:50:21 +0200

    Fix the rootReducer well

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,4 @@
 import { combineReducers } from 'redux';
 import bookReducer from './book';
 
-export default combineReducers({
-  bookReducer,
-});
+export default combineReducers({ bookReducer });
