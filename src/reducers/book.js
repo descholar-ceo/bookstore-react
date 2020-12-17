3e666e802d017e7af44d721a7d7177bff1853b4e
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-17 16:20:35 +0200

    Add bookReducer

---
+++ src/reducers/book.js
@@ -0,0 +1,16 @@
+import { CREATE_BOOK, REMOVE_BOOK } from '../actions/types';
+
+const initialState = {};
+
+const bookReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_BOOK:
+      return { ...state, books: action.payload };
+    case REMOVE_BOOK:
+      return { ...state, removedBook: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default bookReducer;
