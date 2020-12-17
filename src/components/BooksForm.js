a60ddd756dc317ae84a837d32ff33e67471a8473
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-17 15:55:30 +0200

    Fix eslint error and add collection array

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,13 +1,17 @@
-const BooksForm = () => (
-  <div>
-    <form>
-      <input type="text" placeholder="Enter the name of a book" />
-      <select>
-        <option value="">---Select the book category---</option>
-      </select>
-      <button type="button">Save book</button>
-    </form>
-  </div>
-);
+const BooksForm = () => {
+  // eslint-disable-next-line no-unused-vars
+  const categoryCollection = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Enter the name of a book" />
+        <select>
+          <option value="">---Select the book category---</option>
+        </select>
+        <button type="button">Save book</button>
+      </form>
+    </div>
+  );
+};
 
 export default BooksForm;
