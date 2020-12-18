20e38c31f7f91923303f3dac74c57c49595b795a
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-18 10:53:25 +0200

    Refactor codes for consistency

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import randomIdGenerator from '../helpers/randomId';
 import { createBookAction } from '../redux/actions';
 
-const BooksForm = props => {
+const BooksForm = ({ createBook }) => {
   const [state, setState] = useState({ bookTitle: 'default book', bookCategory: 'novel' });
   const handleChange = ({ target: { name, value } }) => setState({ [name]: value });
   const handleSubmit = event => {
     event.preventDefault();
-    console.log('The state is : ', state);
-    console.log('The props are : ', props);
+    createBook({
+      bookId: randomIdGenerator(),
+      bookTitle: state.bookTitle,
+      bookCategory: state.bookCategory,
+    });
   };
   // eslint-disable-next-line no-unused-vars
   const categoryCollection = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
@@ -16,14 +21,26 @@ const BooksForm = props => {
     <div>
       <h2>Create a book</h2>
       <form onSubmit={event => handleSubmit(event)}>
-        <input onChange={event => handleChange(event)} name="bookTitle" type="text" placeholder="Enter the title of a book" />
-        <select onChange={event => handleChange(event)} name="bookCategory">
+        <input
+          onChange={event => handleChange(event)}
+          name="bookTitle"
+          type="text"
+          placeholder="Enter the title of a book"
+        />
+        <select
+          onChange={event => handleChange(event)}
+          name="bookCategory"
+        >
           <option value="default">---Select the book category---</option>
         </select>
         <button type="submit">Save book</button>
       </form>
     </div>
   );
+};
+
+BooksForm.propTypes = {
+  createBook: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = dispatch => ({ createBook: book => dispatch(createBookAction(book)) });
