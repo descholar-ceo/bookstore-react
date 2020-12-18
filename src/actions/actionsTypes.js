fac3b947981a3e6b1a404506509aaa0c1fc58fa1
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-18 09:50:46 +0200

    Put everything related to redux in one folder

--- src/actions/actionsTypes.js
+++
@@ -1,2 +0,0 @@
-export const CREATE_BOOK = 'CREATE_BOOK';
-export const REMOVE_BOOK = 'REMOVE_BOOK';
--- src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux';
-import bookReducer from './book';
-
-export default combineReducers({ bookReducer });
