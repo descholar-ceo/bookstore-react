a65bb20cbeef0bae44773981d35a0a0a4995e1fd
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-20 22:20:46 +0200

    Remove unnecessary props from booklist component

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,7 +1,6 @@
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
-import { createBookAction, removeBookAction } from '../redux/actions';
 
 const BookList = ({ books }) => {
   const bookRows = books.map(book => (<Book key={`book-number-${book.bookId}`} book={book} />));
@@ -34,9 +33,5 @@ BookList.propTypes = {
 };
 
 const mapStateToProps = state => ({ books: state.books });
-const mapDispatchToProps = dispatch => ({
-  createBook: book => dispatch(createBookAction(book)),
-  removeBook: book => dispatch(removeBookAction(book)),
-});
 
-export default connect(mapStateToProps, mapDispatchToProps)(BookList);
+export default connect(mapStateToProps, null)(BookList);
